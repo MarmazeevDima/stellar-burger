fffc32d68dd7168e7ccb8d31d3d420c5d0b72719
Marmazeev Dmitriy <dmarmaz@yandex.ru>
2025-05-10 12:16:56 +0300

    feat: profile-orders

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,16 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { fetchOrders } from '../../services/slices/orders';
+import { useDispatch, useSelector } from '@store';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const { data: orders } = useSelector(store => store.ordersReducer);
+
+	useEffect(() => {
+		dispatch(fetchOrders());
+	}, [dispatch]);
 
   return <ProfileOrdersUI orders={orders} />;
 };
--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,12 +1,12 @@
 import { ProfileUI } from '@ui-pages';
 import { FC, SyntheticEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from '@store';
+import { updateUser } from '../../services/slices/user';
 
 export const Profile: FC = () => {
-  /** TODO: взять переменную из стора */
-  const user = {
-    name: '',
-    email: ''
-  };
+  const dispatch = useDispatch();
+
+  const { data: user } = useSelector(store => store.userReducer);
 
   const [formValue, setFormValue] = useState({
     name: user.name,
@@ -29,6 +29,8 @@ export const Profile: FC = () => {
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    dispatch(updateUser(formValue));
   };
 
   const handleCancel = (e: SyntheticEvent) => {
